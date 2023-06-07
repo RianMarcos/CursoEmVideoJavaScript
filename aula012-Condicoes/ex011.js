d7ee367e61e484a85a4ad5a4c76040c6b20a1437
Rian Marcos <rian.sepulveda@hotmail.com>
2023-06-06 23:27:12 -0300

    If encadeado

---
+++ aula012-Condicoes/ex011.js
@@ -0,0 +1,10 @@
+var idade = 18
+console.log(`você tem ${idade} anos`)
+if(idade < 16){
+    console.log("Não vota")
+}else if (idade >= 16 && idade <18 || idade >65 ){
+    console.log("voto opcional")
+}else{
+    console.log("voto obrigatório")
+}
+    
